9751854da326a1cbad1f9aa6cb1148131099a33e
stark019 <meettonihal@gmail.com>
2020-04-07 20:14:58 +0530

    added put post patch delete

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -3,4 +3,3 @@ from rest_framework import serializers
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing APIViews"""
     name = serializers.CharField(max_length=10)
-    
